2e141be92f857ea79490afc9514ead093a787e7f
Adam Schaal <amschaal@ucdavis.edu>
2018-09-19 16:02:15 -0700

    forgot to add lock.vue from previous commit

---
+++ src/components/lock.vue
@@ -0,0 +1,39 @@
+<template>
+  <div><q-btn label="Unlocked" @click="lock" v-if="!submission.locked" color="green"/><q-btn label="Locked" @click="unlock" v-if="submission.locked" color="red"/></div>
+</template>
+
+<script>
+export default {
+  props: ['submission'],
+  methods: {
+    lock () {
+      var self = this
+      this.$axios.post(`/api/submissions/${this.submission.id}/lock/`)
+        .then(
+          function (response) {
+            self.$q.notify({message: 'Submission locked.', type: 'positive'})
+            self.submission.locked = true
+          })
+        .catch(
+          function () {
+            self.$q.notify({message: 'Error locking submission.', type: 'negative'})
+          }
+        )
+    },
+    unlock () {
+      var self = this
+      this.$axios.post(`/api/submissions/${this.submission.id}/unlock/`)
+        .then(
+          function (response) {
+            self.$q.notify({message: 'Submission unlocked.', type: 'positive'})
+            self.submission.locked = false
+          })
+        .catch(
+          function () {
+            self.$q.notify({message: 'Error unlocking submission.', type: 'negative'})
+          }
+        )
+    }
+  }
+}
+</script>
