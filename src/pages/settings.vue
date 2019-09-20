c1a24e482c303dec46d957cc83c5fd506440a698
adam <amschaal@gmail.com>
2019-09-20 11:23:59 -0700

    simplify lab settings page, use default lab

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -1,15 +1,7 @@
 <template>
   <q-page padding>
-    <q-field
-      label="Select Lab"
-      label-width="2"
-    >
-      <q-select
-        v-model="lab"
-        :options="labOptions"
-      />
-    </q-field>
     <div v-if="lab">
+      <h5>Settings for "{{lab.name}}"</h5>
       <q-field
         label="Home Page"
         label-width="2"
@@ -59,7 +51,7 @@ export default {
   name: 'settings',
   data () {
     return {
-      lab: null,
+      lab: this.$store.getters.lab,
       settings: null,
       labs: [],
       submission_variables: {},
@@ -67,19 +59,13 @@ export default {
     }
   },
   methods: {
-    getLab (lab) {
-      this.lab = lab // this.labs[index]
-    },
     save () {
       var self = this
       if (this.lab.id) {
         this.$axios
           .put(`/api/labs/${this.lab.id}/`, this.lab)
           .then(({ data }) => {
-            if (data.id === self.default_lab.id) {
-              // self.default_lab = data
-              self.$store.commit('lab', data)
-            }
+            self.$store.commit('lab', data)
             self.$q.notify({message: `Settings saved for ${data.name}`, type: 'positive'})
           })
           .catch(error => {
@@ -87,29 +73,6 @@ export default {
             console.log('error', error)
           })
       }
-    }
-  },
-  mounted () {
-    var self = this
-    this.$axios
-      .get(`/api/labs/`)
-      .then(({ data }) => {
-        console.log('data', data)
-        self.labs = data.results
-        if (self.labs.length === 1) {
-          self.lab_id = self.labs[0].id
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  },
-  computed: {
-    default_lab () {
-      return this.$store.getters.lab
-    },
-    labOptions () {
-      return this.labs.map((l, k) => ({label: l.name, value: l}))
     }
   },
   components: {
