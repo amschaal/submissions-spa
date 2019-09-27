a7c1a4f6d9ec4b1369805909314ef5b280ef9044
adam <amschaal@gmail.com>
2019-09-27 13:39:33 -0700

    issue #19, work on adapter samplesheet widgets

--- src/components/aggrid/widgets.js
+++ src/components/aggrid/widgets.js
@@ -5,6 +5,7 @@ import BooleanComponent from './editors/BooleanComponent.vue'
 import VocabularyComponent from './editors/VocabularyComponent.vue'
 import APIAutocompleteComponent from './editors/APIAutocompleteComponent.vue'
 import SelectComponent from './editors/SelectComponent.vue'
+import AdapterAutocompleteComponent from './editors/AdapterAutocompleteComponent.vue'
 import {Widget, WidgetFactory} from '../forms/Widget.js'
 
 class GridWidget extends Widget {
@@ -46,6 +47,17 @@ class VocabularyWidget extends GridWidget {
     {'variable': 'vocabulary_variable', 'label': 'Variable specifying vocabulary', 'type': 'text'}
     // {'variable': 'bar', 'label': 'Bar', 'type': 'text', 'options': [{'label': 'One', 'value': 1}, {'label': 'Two', 'value': 2}]}
   ]
+  validateOptions (options, field, schema) {
+    console.log('hello!!!', options, field, schema)
+    var errors = {}
+    if (options.vocabulary_variable && !schema.properties[options.vocabulary_variable]) {
+      errors['vocabulary_variable'] = [`Variable "${options.vocabulary_variable}" does not exist.`]
+    }
+    if (!options.vocabulary_variable && !options.vocabulary) {
+      errors['vocabulary_variable'] = errors['vocabulary'] = ['You must enter either a vocabulary id, or a variable name where the vocabulary id will be selected.']
+    }
+    return errors
+  }
 }
 
 class APIAutocompleteWidget extends GridWidget {
@@ -61,6 +73,25 @@ class APIAutocompleteWidget extends GridWidget {
     {'variable': 'value_property', 'label': 'Value property', 'type': 'text'},
     {'variable': 'label_property', 'label': 'Label property', 'type': 'text'}
   ]
+}
+
+class AdapterAutocompleteWidget extends APIAutocompleteWidget {
+  // @TODO: wrap this in another component as in the guide https://quasar-framework.org/components/autocomplete.html
+  static type = 'string'
+  static id = 'adapter_autocomplete'
+  static component = APIAutocompleteComponent
+  static name = 'Adapter Autocomplete'
+  static schema =
+  [
+    {'variable': 'db_variable', 'label': 'Variable specifying which adapter database to use', 'type': 'text'}
+  ]
+  validateOptions (options, field, schema) {
+    var errors = {}
+    if (options.vocabulary_variable && !schema.properties[options.vocabulary_variable]) {
+      errors['db_variable'] = [`Variable "${options.vocabulary_variable}" does not exist.`]
+    }
+    return errors
+  }
 }
 
 class APISelectWidget extends APIAutocompleteWidget {
@@ -98,6 +129,6 @@ class AutocompleteWidget extends GridWidget {
   // }
 }
 
-var widgetFactory = new WidgetFactory([DateWidget, BooleanWidget, AutocompleteWidget, VocabularyWidget, APIAutocompleteWidget, APISelectWidget])
+var widgetFactory = new WidgetFactory([DateWidget, BooleanWidget, AutocompleteWidget, VocabularyWidget, APIAutocompleteWidget, APISelectWidget, AdapterAutocompleteWidget])
 
 export default widgetFactory
